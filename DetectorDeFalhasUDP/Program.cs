1041789db0ca377b5d9b2e326227506ede42e2b2
MARCO AURELIO DANIEL <141208@LABINFO.FACENS.BR>
2018-11-28 22:14:17 -0200

    ta pronto, só falta terminar

--- DetectorDeFalhasUDP/Program.cs
+++ DetectorDeFalhasUDP/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -11,26 +10,9 @@ namespace DetectorDeFalhasUDP
 {
     public class Program
     {
-        private static readonly int SEND_PORT = 6001;
-        private static readonly int RECEIVE_PORT = 6000;
-
-        private static readonly string REQUEST_MESSAGE = "HeartbeatRequest";
-        private static readonly string REPLY_MESSAGE = "HeartbeatReply";
-        private static readonly string PROCESS_REQUEST_MESSAGE = "ProcessRequest";
-        private static readonly string PROCESS_INTERRUPT_MESSAGE = "ProcessInterruptRequest";
-        private static readonly string PROCESS_ANSWER_NO_MESSAGE = "ProcessAnswerNo";
-        private static readonly string PROCESS_ANSWER_YES_MESSAGE = "ProcessAnswerYes;{0}";
-        private static readonly string PROCESS_MESSAGE = "Process;{0};{1}";
-
-        private static volatile List<string> all = new List<string>();
-        private static volatile List<string> live = new List<string>();
-        private static volatile List<string> dead = new List<string>();
-        private static volatile string leader = string.Empty;
-
-        private static volatile Mutex mutex = new Mutex();
-
         public static void Main(string[] args)
         {
+            all = new List<string>(all);
             all.Add("172.18.2.217");
             all.Add("172.18.3.162");
             all.Add("172.18.1.41");
@@ -38,30 +20,52 @@ namespace DetectorDeFalhasUDP
             all.Add("172.17.128.81");
             all.Add("172.18.3.79");
             all.Add("172.18.3.78");
-            
-            live.AddRange(all);
+            live = new List<string>(all);
+            dead = new List<string>();
+
             leader = live.FirstOrDefault();
 
-            new Thread(() =>
-            {
-                Sender();
-            }).Start();
-            new Thread(() =>
-            {
-                Receiver();
-            }).Start();
+            new Thread(() => Sender()).Start();
+            new Thread(() => Receiver()).Start();
         }
+
+        private static readonly int SEND_PORT = 6001;
+        private static readonly int RECEIVE_PORT = 6000;
+
+        private static readonly string REQUEST_MESSAGE = "HeartbeatRequest";
+        private static readonly string REPLY_MESSAGE = "HeartbeatReply";
+
+        private static readonly string PROCESS_REQUEST_MESSAGE = "ProcessRequest";
+        private static readonly string PROCESS_INTERRUPT_MESSAGE = "ProcessInterruptRequest";
+        private static readonly string PROCESS_ANSWER_NO_MESSAGE = "ProcessAnswerNo";
+        private static readonly string PROCESS_ANSWER_YES_MESSAGE = "ProcessAnswerYes;{0}";
+        private static readonly string PROCESS_MESSAGE = "Process;";
+
+        private static volatile List<string> all;
+        private static volatile List<string> live;
+        private static volatile List<string> dead;
+        private static string leader = string.Empty;
+
+        private static volatile Mutex mutexFailure = new Mutex();
+        private static bool isIdle = true;
 
         private static void Sender()
         {
             while (true)
             {
-                mutex.WaitOne();
+                mutexFailure.WaitOne();
                 try
                 {
                     dead.RemoveAll(x => live.Contains(x));
                     var except = all.Except(live).ToList();
-                    except.ForEach(x => DeadHost(x));
+                    except.ForEach(ip =>
+                    {
+                        if (!dead.Contains(ip))
+                        {
+                            dead.Add(ip);
+                            Console.WriteLine(string.Format("Host {0} parou de responder!", ip));
+                        }
+                    });
                     leader = all.Except(dead).FirstOrDefault();
                     Console.WriteLine(string.Format("\n\n***** LIDER: {0} - {1} ****\n\n", all.IndexOf(leader) + 1, leader));
 
@@ -70,6 +74,11 @@ namespace DetectorDeFalhasUDP
                         SendMessage(ip, REQUEST_MESSAGE);
                     }
                     live.Clear();
+                    if (isIdle)
+                    {
+                        isIdle = false;
+                        SendMessage(leader, PROCESS_REQUEST_MESSAGE);
+                    }
                 }
                 catch (Exception e)
                 {
@@ -77,7 +86,7 @@ namespace DetectorDeFalhasUDP
                 }
                 finally
                 {
-                    mutex.ReleaseMutex();
+                    mutexFailure.ReleaseMutex();
                     Thread.Sleep(2000);
                 }
             }
@@ -89,7 +98,7 @@ namespace DetectorDeFalhasUDP
             IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, RECEIVE_PORT);
             while (true)
             {
-                mutex.WaitOne();
+                mutexFailure.WaitOne();
                 try
                 {
                     Byte[] receiveBytes = receivingUdpClient.Receive(ref RemoteIpEndPoint);
@@ -101,12 +110,16 @@ namespace DetectorDeFalhasUDP
                         Console.WriteLine(string.Format("Respondido {0} para {1}", REPLY_MESSAGE, ip));
                         SendMessage(ip, REPLY_MESSAGE);
                     }
-                    else if (message == REPLY_MESSAGE)
+                    if (message == REPLY_MESSAGE)
                     {
                         Console.WriteLine(string.Format("Host {0} respondeu {1}", ip, REPLY_MESSAGE));
                         live.Add(ip);
                         if (!dead.Contains(ip))
                             dead.Remove(ip);
+                    }
+                    if (message.Contains(PROCESS_MESSAGE))
+                    {
+                        // TODO
                     }
                 }
                 catch (Exception e)
@@ -115,19 +128,9 @@ namespace DetectorDeFalhasUDP
                 }
                 finally
                 {
-                    mutex.ReleaseMutex();
+                    mutexFailure.ReleaseMutex();
                 }
             }
-        }
-
-        private static void DeadHost(string ip)
-        {
-            if (!dead.Contains(ip))
-            {
-                dead.Add(ip);
-                Console.WriteLine(string.Format("Host {0} parou de responder!", ip));
-            }
-
         }
 
         private static void SendMessage(string ip, string message)
